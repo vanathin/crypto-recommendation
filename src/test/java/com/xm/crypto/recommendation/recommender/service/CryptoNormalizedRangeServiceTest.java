2cca4c4fbab15e44b363e39b17b2665f93fbb4e6
vanathin <vanudevi@gmail.com>
2023-08-01 15:10:42 +0400

    test case for CryptoNormalizedRangeService

--- src/test/java/com/xm/crypto/recommendation/recommender/service/CryptoNormalizedRangeServiceTest.java
+++ src/test/java/com/xm/crypto/recommendation/recommender/service/CryptoNormalizedRangeServiceTest.java
@@ -1,8 +1,8 @@
 package com.xm.crypto.recommendation.recommender.service;
 
 import com.xm.crypto.recommendation.recommender.dto.CryptoNormalizedRangeDTO;
+import com.xm.crypto.recommendation.recommender.dto.CryptoRecommenderResponseDTO;
 import com.xm.crypto.recommendation.recommender.persistence.repository.CryptoNormalizedRangeRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,8 +10,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -19,43 +23,110 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class CryptoNormalizedRangeServiceTest {
 
+
     @Mock
     private CryptoNormalizedRangeRepository cryptoNormalizedRangeRepository;
 
     @InjectMocks
     private CryptoNormalizedRangeService cryptoNormalizedRangeService;
 
-    @BeforeEach
-    public void setup() {
-        // Setup mock data for the repository
-        List<Object[]> mockResults = Arrays.asList(
-                new Object[] { "BTC", BigDecimal.valueOf(100) },
-                new Object[] { "ETH", BigDecimal.valueOf(50) },
-                new Object[] { "XRP", BigDecimal.valueOf(200) }
-        );
+    @Test
+    public void testFindCryptoWithNormalizedRange() {
+        // Given
+        Object[] row1 = {"BTC", BigDecimal.valueOf(0.25)};
+        Object[] row2 = {"ETH", BigDecimal.valueOf(0.35)};
+        List<Object[]> results = Arrays.asList(row1, row2);
+        when(cryptoNormalizedRangeRepository.findCryptoWithNormalizedRange()).thenReturn(results);
 
-        // Mock the repository method call
-        when(cryptoNormalizedRangeRepository.findCryptoWithNormalizedRange()).thenReturn(mockResults);
+        // When
+        List<CryptoNormalizedRangeDTO> dtoList = cryptoNormalizedRangeService.findCryptoWithNormalizedRange();
+
+        // Then
+        assertEquals(2, dtoList.size());
+        assertEquals("BTC", dtoList.get(0).getSymbol());
+        assertEquals(BigDecimal.valueOf(0.25), dtoList.get(0).getNormalizedRange());
+        assertEquals("ETH", dtoList.get(1).getSymbol());
+        assertEquals(BigDecimal.valueOf(0.35), dtoList.get(1).getNormalizedRange());
     }
 
     @Test
-    public void testFindCryptoWithNormalizedRange() {
+    public void testFindCryptoWithNormalizedRange_NoResults() {
+        // Given
+        when(cryptoNormalizedRangeRepository.findCryptoWithNormalizedRange()).thenReturn(null);
+
         // When
-        List<CryptoNormalizedRangeDTO> result = cryptoNormalizedRangeService.findCryptoWithNormalizedRange();
+        List<CryptoNormalizedRangeDTO> dtoList = cryptoNormalizedRangeService.findCryptoWithNormalizedRange();
 
         // Then
-        assertNotNull(result);
-        assertEquals(3, result.size());
+        assertTrue(dtoList.isEmpty());
+    }
 
-        // Assert individual DTO elements (assuming the order is preserved)
-        assertEquals("BTC", result.get(0).getSymbol());
-        assertEquals(BigDecimal.valueOf(100), result.get(0).getNormalizedRange());
+    @Test
+    public void testFindCryptoWithNormalizedRange_WithResults() {
+        // Given
+        Object[] result1 = { "BTC", new BigDecimal("0.25") };
+        Object[] result2 = { "ETH", new BigDecimal("0.15") };
+        List<Object[]> results = Arrays.asList(result1, result2);
 
-        assertEquals("ETH", result.get(1).getSymbol());
-        assertEquals(BigDecimal.valueOf(50), result.get(1).getNormalizedRange());
+        when(cryptoNormalizedRangeRepository.findCryptoWithNormalizedRange()).thenReturn(results);
 
-        assertEquals("XRP", result.get(2).getSymbol());
-        assertEquals(BigDecimal.valueOf(200), result.get(2).getNormalizedRange());
+        // When
+        List<CryptoNormalizedRangeDTO> dtoList = cryptoNormalizedRangeService.findCryptoWithNormalizedRange();
+
+        // Then
+        assertFalse(dtoList.isEmpty());
+        assertEquals(2, dtoList.size());
+
+        CryptoNormalizedRangeDTO dto1 = dtoList.get(0);
+        assertEquals("BTC", dto1.getSymbol());
+        assertEquals(new BigDecimal("0.25"), dto1.getNormalizedRange());
+
+        CryptoNormalizedRangeDTO dto2 = dtoList.get(1);
+        assertEquals("ETH", dto2.getSymbol());
+        assertEquals(new BigDecimal("0.15"), dto2.getNormalizedRange());
+    }
+
+
+    @Test
+    public void testFindCryptoWithHighestNormalizedRange() {
+        // Given
+        LocalDate date = LocalDate.of(2023, 7, 31);
+        LocalDateTime startDateTime = date.atStartOfDay();
+        LocalDateTime endDateTime = date.plusDays(1).atStartOfDay();
+
+        Object[] row = {"BTC", BigDecimal.valueOf(0.25)};
+        List<Object[]> listOfArrays = new ArrayList<>();
+        listOfArrays.add(row);
+
+        when(cryptoNormalizedRangeRepository.findCryptoWithHighestNormalizedRangeForSpecificDay(startDateTime, endDateTime))
+                .thenReturn(listOfArrays);
+
+        // When
+        Optional<CryptoRecommenderResponseDTO> response = cryptoNormalizedRangeService.findCryptoWithHighestNormalizedRange(date);
+
+        // Then
+        assertTrue(response.isPresent());
+        assertEquals("BTC", response.get().getSymbol());
+        assertEquals(BigDecimal.valueOf(0.25), response.get().getHighestNormalizedRange());
+    }
+
+    @Test
+    public void testFindCryptoWithHighestNormalizedRange_NoResult() {
+        // Given
+        LocalDate date = LocalDate.of(2023, 7, 31);
+        LocalDateTime startDateTime = date.atStartOfDay();
+        LocalDateTime endDateTime = date.plusDays(1).atStartOfDay();
+
+        List<Object[]> result = new ArrayList<>();
+
+        when(cryptoNormalizedRangeRepository.findCryptoWithHighestNormalizedRangeForSpecificDay(startDateTime, endDateTime))
+                .thenReturn(result);
+
+        // When
+        Optional<CryptoRecommenderResponseDTO> response = cryptoNormalizedRangeService.findCryptoWithHighestNormalizedRange(date);
+
+        // Then
+        assertFalse(response.isPresent());
     }
 
 }
